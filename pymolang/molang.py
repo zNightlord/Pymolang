ff4f8af6debb132ec92520cd39259dcd65790e48
0znightlord0@gmail.com <0znightlord0@gmail.com>
2023-10-01 16:12:27 +0700

    ghggs

--- pymolang/molang.py
+++ pymolang/molang.py
@@ -195,7 +195,7 @@ class Scanner():
         if self.peek() == '.':
             # if self.peek_next().isalpha():
             #     # Syntax like 5.sin()
-                return self.make_token(TokenType.INT)
+            #     return self.make_token(TokenType.INT)
             self.advance()
             return self.float()
         return self.make_token(TokenType.FLOAT)
